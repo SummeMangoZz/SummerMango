a540b3aec56a716f524dc425ce32e74b659588d0
SummerMangoZz <m18797815710@163.com>
2017-02-22 15:37:36 +0800

    添加注释

--- DesignPatterns/src/BuilderPattern/App.java
+++ DesignPatterns/src/BuilderPattern/App.java
@@ -4,11 +4,10 @@ public class App {
 	public static void main(String[] args) {
 		ConcreteBuilderA concreteBuilderA = new ConcreteBuilderA();
 		ConcreteBuilderB concreteBuilderB = new ConcreteBuilderB();
+		//负责指挥具体的建造过程
 		Director directorA = new Director(concreteBuilderA);
 		Director directorB = new Director(concreteBuilderB);
 		directorA.buildProduct();
 		directorB.buildProduct();
-		
-		
 	}
 }
