44d0f233c2b25ae757be413b128eb15975919eb8
SummerMangoZz <m18797815710@163.com>
2017-02-22 20:54:38 +0800

    增加注释

--- DesignPatterns/src/BuilderPattern/Director.java
+++ DesignPatterns/src/BuilderPattern/Director.java
@@ -5,6 +5,11 @@ package BuilderPattern;
  * @author w
  *
  */
+/**
+ * 用这个类来控制建造过程
+ * @author w
+ *
+ */
 public class Director {
 	 private Builder builder;
 	 
@@ -12,6 +17,12 @@ public class Director {
 		this.builder = builder; 
 	 }
 	 
+	 /**
+	  * 建造过程被确定下来，但各个部分的建造细节并不一样，因此可以根据相同的建造过程，通过建造细节的不同，
+	  * 创建不同表示的对象
+	  * 建造者模式就是将一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示
+	  * 
+	  */
 	 public void buildProduct(){
 		 builder.buildPartA();
 		 builder.buildPartB();
