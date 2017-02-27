a0f3b29ff891e47d92f5047dbeaa5968a8dd9b48
SummerMangoZz <m18797815710@163.com>
2017-02-27 09:25:24 +0800

    增加注释

--- DesignPatterns/src/StrategyPattern/CushSuperFactory.java
+++ DesignPatterns/src/StrategyPattern/CushSuperFactory.java
@@ -7,6 +7,7 @@ public class CushSuperFactory{
 	private static Map<String,CushSuper> cushMap = new HashMap<String,CushSuper>();
 	static{
 		//利用map来取消if-else语句和switch语句
+		//如果要增加一种算法的话，还是要修改这段代码的，所以并不完全符合开必原则
 		cushMap.put("1", new CushRebate(1));
 		cushMap.put("2", new CushRebate(0.8));
 		cushMap.put("3", new CushReturn(300,100));
